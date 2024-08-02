e8e60b9375038f79197607c9a20db7ac8ce8363b
keita-sa <keitasakuraims@gmail.com>
2024-08-02 20:55:04 +0900

    changed webdriver.Chrome

--- S4/L27/tests/test_login_page_l27.py
+++ S4/L27/tests/test_login_page_l27.py
@@ -1,11 +1,12 @@
 # Open browser
 # selenium 4
 import time
-import pytest
 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 # from webdriver_manager.chrome import ChromeDriverManager
 
